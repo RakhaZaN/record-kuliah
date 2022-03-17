9996e49bff09b83dadbfc715eb046785609eb7f6
rakhazan <rakhazahnur26@gmail.com>
2022-03-17 20:32:16 +0700

    :sparkles: Semester 2 - Praktikum 3

--- Semester-2/form/nilai_siswa.php
+++ Semester-2/form/nilai_siswa.php
@@ -20,7 +20,57 @@ include_once '../template/atas.php';
 
 <div class="container py-5">
 
-
+    <div class="card">
+        <div class="card-header">
+            <h4 class="card-title">Nilai Siswa</h4>
+        </div>
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table">
+                    <tr>
+                        <th>Nama</th>
+                        <td>:</td>
+                        <td><?= $nama; ?></td>
+                    </tr>
+                    <tr>
+                        <th>Matkul</th>
+                        <td>:</td>
+                        <td><?= $matkul; ?></td>
+                    </tr>
+                    <tr>
+                        <th>UTS</th>
+                        <td>:</td>
+                        <td><?= $uts; ?></td>
+                    </tr>
+                    <tr>
+                        <th>UAS</th>
+                        <td>:</td>
+                        <td><?= $uas; ?></td>
+                    </tr>
+                    <tr>
+                        <th>Tugas</th>
+                        <td>:</td>
+                        <td><?= $tugas; ?></td>
+                    </tr>
+                    <tr>
+                        <th>Status</th>
+                        <td>:</td>
+                        <td><?= $status; ?></td>
+                    </tr>
+                    <tr>
+                        <th>Grade</th>
+                        <td>:</td>
+                        <td><?= $grade; ?></td>
+                    </tr>
+                    <tr>
+                        <th>Predikat</th>
+                        <td>:</td>
+                        <td><?= $predikat; ?></td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </div>
 
 </div>
 
